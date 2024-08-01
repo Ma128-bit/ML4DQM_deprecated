2de9e289de2cdcdb4e49224779d454d4edcf8532
Quibusque <cruck990@gmail.com>
2024-08-01 10:15:37 +0200

    re-order ResNet.py to make things more readable and the process clearer I tested and it looks like nothing broke and the learning still works exactly the same

--- ResNet.py
+++ ResNet.py
@@ -9,17 +9,193 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from torchvision import datasets, transforms
 
+
+class ResNetAE(nn.Module):
+    """
+    Define the full ResNet autoencoder model
+    """
+
+    def __init__(self, in_channels, nblocks, fmaps, img_size):
+        super(ResNetAE, self).__init__()
+
+        self.fmaps = fmaps
+        self.nblocks = nblocks
+        self.in_channels = in_channels
+        self.img_size = img_size
+
+        self.debug = False
+        self._initialize_encoding_layers(in_channels, fmaps)
+        self._initialize_sizes(img_size)
+        self._initialize_decoding_layers(in_channels, fmaps)
+
+    def _initialize_encoding_layers(self, in_channels, fmaps):
+        # Initialize encoding layers
+        self.econv0 = nn.Sequential(
+            nn.Conv2d(in_channels, fmaps[0], kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+        )
+        self.elayer1 = self.block_layers(self.nblocks, [fmaps[0], fmaps[0]], "enc")
+        self.elayer2 = self.block_layers(1, [fmaps[0], fmaps[1]], "enc")
+        self.elayer3 = self.block_layers(self.nblocks, [fmaps[1], fmaps[1]], "enc")
+        self.elayer4 = self.block_layers(1, [fmaps[1], fmaps[2]], "enc")
+        self.elayer5 = self.block_layers(self.nblocks, [fmaps[2], fmaps[2]], "enc")
+
+    def _initialize_sizes(self, img_size):
+        # initialize the sizes across the layers
+        self.size0 = list(img_size)
+        self.size2 = [int(np.floor(el * 0.5)) for el in self.size0]
+        self.size4 = [int(np.ceil(el * 0.5)) for el in self.size2]
+        self.size5 = [int(np.ceil(el * 0.5)) for el in self.size4]
+        if self.debug:
+            print(f"size0: {self.size0}")
+            print(f"size2: {self.size2}")
+            print(f"size4: {self.size4}")
+            print(f"size5: {self.size5}")
+
+    def _initialize_decoding_layers(self, in_channels, fmaps):
+        # Initialize decoding layers
+        self.fc = nn.Linear(
+            self.fmaps[-1], self.fmaps[-1] * self.size5[0] * self.size5[1]
+        )  # 5x5
+        self.dlayer5 = self.block_layers(
+            self.nblocks, [fmaps[2], fmaps[2]], "dec", out_shape=None
+        )
+        self.dlayer4 = self.block_layers(
+            1, [fmaps[2], fmaps[1]], "dec", out_shape=self.size4
+        )
+        self.dlayer3 = self.block_layers(
+            self.nblocks, [fmaps[1], fmaps[1]], "dec", out_shape=None
+        )
+        self.dlayer2 = self.block_layers(
+            1, [fmaps[1], fmaps[0]], "dec", out_shape=self.size2
+        )
+        self.dlayer1 = self.block_layers(
+            self.nblocks, [fmaps[0], fmaps[0]], "dec", out_shape=None
+        )
+        self.dconv0 = nn.ConvTranspose2d(
+            fmaps[0], in_channels, kernel_size=3, stride=1, padding=(1, 1)
+        )
+        self.dconv0_relu = nn.ReLU(inplace=True)
+
+    def block_layers(self, nblocks, fmaps, state, out_shape=None):
+        """
+        Convenience function: append several resnet blocks in sequence
+        """
+        layers = []
+        for _ in range(nblocks):
+            if state == "enc":
+                layers.append(ResBlock(fmaps[0], fmaps[1]))
+            else:
+                layers.append(ResBlockTranspose(fmaps[0], fmaps[1], out_shape))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+
+        x = self.encode(x)
+        x = self.decode(x)
+
+        return x
+
+    def encode(self, x):
+        if self.debug:
+            print(x.size())
+        if self.debug:
+            print("Encode")
+        x = self.econv0(x)
+        if self.debug:
+            print(x.size())
+        x = F.max_pool2d(x, kernel_size=2)
+        if self.debug:
+            print(x.size())
+
+        x = self.elayer1(x)
+        if self.debug:
+            print(x.size())
+        x = self.elayer2(x)
+        if self.debug:
+            print(x.size())
+        x = self.elayer3(x)
+        if self.debug:
+            print(x.size())
+        x = self.elayer4(x)
+        if self.debug:
+            print(x.size())
+        x = self.elayer5(x)
+        if self.debug:
+            print(x.size())
+
+        # Bottleneck comes from GlobalMaxPool
+        if self.debug:
+            print("Maxpool-FC")
+        x = F.max_pool2d(x, kernel_size=x.size()[2:])
+        if self.debug:
+            print(x.size())
+        x = x.view(x.size()[0], -1)
+        if self.debug:
+            print(x.size())
+
+        return x
+
+    def decode(self, x):
+        # Expand bottleneck
+        # Dimensions follow encoding steps in reverse, as much as possible
+        x = self.fc(x)  # expand
+        if self.debug:
+            print(x.size())
+        x = x.view(-1, self.fmaps[-1], self.size5[0], self.size5[1])
+        if self.debug:
+            print(x.size())
+
+        # Decoding
+        if self.debug:
+            print("Decode")
+        x = self.dlayer5(x)
+        if self.debug:
+            print(x.size())
+        x = self.dlayer4(x)
+        if self.debug:
+            print(x.size())
+        x = self.dlayer3(x)
+        if self.debug:
+            print(x.size())
+        x = self.dlayer2(x)
+        if self.debug:
+            print(x.size())
+        x = self.dlayer1(x)
+        if self.debug:
+            print(x.size())
+
+        x = F.interpolate(x, size=list(self.size0))
+        if self.debug:
+            print(x.size())
+        x = self.dconv0(
+            x, output_size=(x.size()[0], self.in_channels, self.size0[0], self.size0[1])
+        )
+        if self.debug:
+            print(x.size())
+        x = self.dconv0_relu(x)
+        if self.debug:
+            print(x.size())
+
+        return x
+
+
 class ResBlock(nn.Module):
-    '''
+    """
     For encoding, define the nominal resnet block
-    '''
+    """
+
     def __init__(self, in_channels, out_channels):
         super(ResBlock, self).__init__()
-        self.downsample = out_channels//in_channels
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=self.downsample, padding=1)
+        self.downsample = out_channels // in_channels
+        self.conv1 = nn.Conv2d(
+            in_channels, out_channels, kernel_size=3, stride=self.downsample, padding=1
+        )
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.shortcut = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=self.downsample)
+        self.shortcut = nn.Conv2d(
+            in_channels, out_channels, kernel_size=1, stride=self.downsample
+        )
 
     def forward(self, x):
         residual = x
@@ -36,27 +212,43 @@ class ResBlock(nn.Module):
 
         return out
 
+
 class ResBlockTranspose(nn.Module):
-    '''
+    """
     For decoding, define the transposed resnet block, aka "de-convolution"
-    '''
+    """
+
     def __init__(self, in_channels, out_channels, out_shape=None):
         super(ResBlockTranspose, self).__init__()
-        self.upsample = in_channels//out_channels  
-        self.convT1 = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=self.upsample, padding=1)
+        self.upsample = in_channels // out_channels
+        self.convT1 = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size=3, stride=self.upsample, padding=1
+        )
         self.relu = nn.ReLU(inplace=True)
-        self.convT2 = nn.ConvTranspose2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.convT2 = nn.ConvTranspose2d(
+            out_channels, out_channels, kernel_size=3, padding=1
+        )
 
-        self.shortcutT = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=1, stride=self.upsample)
+        self.shortcutT = nn.ConvTranspose2d(
+            in_channels, out_channels, kernel_size=1, stride=self.upsample
+        )
         self.out_shape = out_shape
 
     def forward(self, x):
         residual = x
-        
+
         if self.out_shape is None:
             out = self.convT1(x)
         else:
-            out = self.convT1(x, output_size=(x.size()[0], x.size()[1], self.out_shape[0], self.out_shape[1]))
+            out = self.convT1(
+                x,
+                output_size=(
+                    x.size()[0],
+                    x.size()[1],
+                    self.out_shape[0],
+                    self.out_shape[1],
+                ),
+            )
         out = self.relu(out)
         out = self.convT2(out)
 
@@ -64,124 +256,17 @@ class ResBlockTranspose(nn.Module):
             if self.out_shape is None:
                 residual = self.shortcutT(x)
             else:
-                residual = self.shortcutT(x, output_size=(x.size()[0], x.size()[1], self.out_shape[0], self.out_shape[1]))
-            
+                residual = self.shortcutT(
+                    x,
+                    output_size=(
+                        x.size()[0],
+                        x.size()[1],
+                        self.out_shape[0],
+                        self.out_shape[1],
+                    ),
+                )
+
         out += residual
         out = self.relu(out)
 
         return out
-
-class ResNetAE(nn.Module):
-    '''
-    Define the full ResNet autoencoder model
-    '''
-    def __init__(self, in_channels, nblocks, fmaps, img_size):
-        super(ResNetAE, self).__init__()
-
-        self.fmaps = fmaps
-        self.nblocks = nblocks
-        self.in_channels = in_channels
-        
-        self.debug = False
-
-        # Initialize encoding layers
-        self.econv0 = nn.Sequential(nn.Conv2d(in_channels, fmaps[0], kernel_size=3, stride=1, padding=1), nn.ReLU())
-        self.elayer1 = self.block_layers(self.nblocks, [fmaps[0],fmaps[0]], 'enc')
-        self.elayer2 = self.block_layers(1, [fmaps[0],fmaps[1]], 'enc')
-        self.elayer3 = self.block_layers(self.nblocks, [fmaps[1],fmaps[1]], 'enc')
-        self.elayer4 = self.block_layers(1, [fmaps[1],fmaps[2]], 'enc')
-        self.elayer5 = self.block_layers(self.nblocks, [fmaps[2],fmaps[2]], 'enc')
-        
-        #initialize the sizes across the layers
-        self.size0 = list(img_size)
-        self.size2 = [int(np.floor(el*0.5)) for el in self.size0]
-        self.size4 = [int(np.ceil(el*0.5)) for el in self.size2]
-        self.size5 = [int(np.ceil(el*0.5)) for el in self.size4]
-        
-        if self.debug:
-            print(f"size0: {self.size0}")
-            print(f"size2: {self.size2}")
-            print(f"size4: {self.size4}")
-            print(f"size5: {self.size5}")
-
-        # Initialize decoding layers
-        self.fc = nn.Linear(self.fmaps[-1], self.fmaps[-1]*self.size5[0]*self.size5[1]) # 5x5
-        self.dlayer5 = self.block_layers(self.nblocks, [fmaps[2],fmaps[2]], 'dec', out_shape=None)
-        self.dlayer4 = self.block_layers(1, [fmaps[2],fmaps[1]], 'dec', out_shape=self.size4)
-        self.dlayer3 = self.block_layers(self.nblocks, [fmaps[1],fmaps[1]], 'dec', out_shape=None)
-        self.dlayer2 = self.block_layers(1, [fmaps[1],fmaps[0]], 'dec', out_shape=self.size2)
-        self.dlayer1 = self.block_layers(self.nblocks, [fmaps[0],fmaps[0]], 'dec', out_shape=None)
-        self.dconv0 = nn.ConvTranspose2d(fmaps[0], in_channels, kernel_size=3, stride=1, padding=(1,1))
-        self.dconv0_relu = nn.ReLU(inplace=True)
-        
-    def block_layers(self, nblocks, fmaps, state, out_shape=None):
-        '''
-        Convenience function: append several resnet blocks in sequence
-        '''
-        layers = []
-        for _ in range(nblocks):
-            if state == 'enc':
-                layers.append(ResBlock(fmaps[0], fmaps[1]))
-            else:
-                layers.append(ResBlockTranspose(fmaps[0], fmaps[1], out_shape)) 
-        return nn.Sequential(*layers)
-
-
-    def forward(self, x):
-        
-        # Encoding
-        if self.debug: print(x.size())
-        if self.debug: print("Encode")
-        x = self.econv0(x)
-        if self.debug: print(x.size())
-        x = F.max_pool2d(x, kernel_size=2)
-        if self.debug: print(x.size())
-
-        x = self.elayer1(x)
-        if self.debug: print(x.size())
-        x = self.elayer2(x)
-        if self.debug: print(x.size())
-        x = self.elayer3(x)
-        if self.debug: print(x.size()) #32x9x18
-        x = self.elayer4(x)
-        if self.debug: print(x.size())
-        x = self.elayer5(x)
-        if self.debug: print(x.size())
-        
-        # Bottleneck comes from GlobalMaxPool
-        if self.debug: print("Maxpool-FC")
-        x = F.max_pool2d(x, kernel_size=x.size()[2:])
-        if self.debug: print(x.size())
-        x = x.view(x.size()[0], -1)
-        if self.debug: print(x.size())
-        
-        # Expand bottleneck
-        # Dimensions follow encoding steps in reverse, as much as possible
-        x = self.fc(x) # expand   
-        if self.debug: print(x.size())
-        #x = x.view(-1, self.fmaps[1], 4, 9) 
-        #x = x.view(-1, self.fmaps[1], 9, 18) 
-        x = x.view(-1, self.fmaps[-1], self.size5[0],self.size5[1])
-        if self.debug: print(x.size())        
-        
-        # Decoding
-        if self.debug: print("Decode")
-        x = self.dlayer5(x)
-        if self.debug: print(x.size())
-        x = self.dlayer4(x)
-        if self.debug: print(x.size())
-        x = self.dlayer3(x)
-        if self.debug: print(x.size())
-        x = self.dlayer2(x)
-        if self.debug: print(x.size())
-        x = self.dlayer1(x)
-        if self.debug: print(x.size())
-        
-        x = F.interpolate(x, size=list(self.size0))
-        if self.debug: print(x.size())
-        x = self.dconv0(x, output_size=(x.size()[0], self.in_channels, self.size0[0], self.size0[1]))
-        if self.debug: print(x.size())
-        x = self.dconv0_relu(x)
-            
-        return x
-        #return x, mu, logvar
